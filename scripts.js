0a4166d434ee2c8132b175d9f6d80df3917720b8
Nicolo <lukakuchaidze@gmail.com>
2023-06-07 20:16:07 +0400

    .

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleNavbar() {
+    var navbarToggle = document.querySelector('.navbar-toggle');
+    var navbarLinks = document.querySelector('.navbar-links');
+    navbarToggle.classList.toggle('active');
+    navbarLinks.classList.toggle('active');
+}
